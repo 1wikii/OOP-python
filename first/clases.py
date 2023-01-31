2dd21aae48d4df82f69f20784d87cf6837252331
ahmaddwiky <zerro140766@gmail.com>
2023-01-31 07:19:47 +0700

    up

--- first/clases.py
+++ first/clases.py
@@ -1,12 +1,14 @@
 class item:
+
+    discount  = 0.8
+
     def __init__(self, name: str, price: float, quan=0):   # quan=0 mean we can empty quan while calling this class
-        												   # name: str mean parameter only take string input
+        											        # name: str mean parameter only take string input
         #checking eror with assert
         assert price >= 0, f"price {price} is not valid!"
         assert quan >= 0, f"quantity {quan} is not valid!"
         
-        
-        #assign to self object
+        #assign to self
         print(f"Name of product : {name}")              
         self.name = name
         self.price = price
@@ -15,11 +17,6 @@ class item:
     def calculate_total(self):
         return self.price*self.quan
 
+    def Discount(self):
+       return self.price * item.discount
 
-#Main
-x = item("android",1000,10)
-
-y = item("pc",2000,10)
-
-print("price = ",end="")
-print(x.calculate_total())
---
+++ first/object.py
@@ -0,0 +1,15 @@
+from clases import *
+
+
+#Main
+x = item("android",1000,10)
+
+y = item("pc",2000,10)
+
+print("price = ",end="")
+#print(x.calculate_total())
+print(x.Discount())
+
+#acces class attribute
+#rint(item.discount)
+#print(x.discount)
